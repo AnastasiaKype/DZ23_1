e5733341b46fe38067ff5a68711399fca79096ad
AnastasiaKype <black_cat123@mail.ru>
2022-03-28 19:29:35 +0300

    redid code

--- src/main/java/GameManager.java
+++ src/main/java/GameManager.java
@@ -21,7 +21,7 @@ public class GameManager {
         int sum = 0;
 
         for (int round : speeds) {
-            if (game.isFailed(round) == false) {
+            if (!game.isFailed(round)) {
                 sum++;
 
             } else {
@@ -34,25 +34,20 @@ public class GameManager {
     public int loser(Movable p1, Movable p2, Game game, int rounds) {
         int i = 0;
         int x = 0;
-        if (game.getIsGreenLight()) {
-            x = 0;
-        } else {
+        while (i != rounds) {
             if (game.isFailed(p1.getSpeed())) {
-                while (i != rounds) {
-                    if (game.isFailed(p2.getSpeed())) {
-                        i++;
-                    } else {
-                        x = 1;
-                        break;
-                    }
-                }
-            } else {
-                if (!game.isFailed(p2.getSpeed())) {
+                if (game.isFailed(p2.getSpeed())) {
                     x = 0;
                 } else {
                     x = -1;
                 }
+                break;
+            } else {
+                if (!game.isFailed(p2.getSpeed())) {
+                    x = 1;
+                }
             }
+            i++;
         }
         return x;
     }
--- src/test/java/MovableTest.java
+++ src/test/java/MovableTest.java
@@ -17,7 +17,7 @@ public class MovableTest {
     @Test
     public void greenLightTest() {
         GameManager gameManager = new GameManager(new Game(true));
-        int expected = 0;
+        int expected = 1;
         int actual = gameManager.loser(new FastPlayer(0,1), new FastPlayer(0, 1), new Game(true), 5);
         Assertions.assertEquals(expected, actual);
 
